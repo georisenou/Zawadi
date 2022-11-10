3615c0707396160bf1418236d170d64a78d3491e
georis <georiseno@gmail.com>
2022-11-10 07:32:41 +0100

    Newn way

--- Zawadi/settings.her.py
+++ Zawadi/settings.her.py
@@ -16,7 +16,6 @@ import cloudinary_storage
 from pyparsing import os
 import firebase_admin
 from firebase_admin import credentials
-import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
@@ -35,9 +34,8 @@ if os.environ.get('ENV') == "PRODUCTION" :
 else :
     DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["zawadi-proj.herokuapp.com", "zawadi.site", "www.zawadi.site" ,"vendeur.zawadi.site"]
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
 
@@ -100,19 +98,16 @@ ASGI_APPLICATION = 'MediaProj.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
+}
+
+if os.environ.get("ENV") == "PRODUCTION" :
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
