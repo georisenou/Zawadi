2e70347af2a926fd68a2729304de73db0f4ae168
georis <georiseno@gmail.com>
2022-12-03 21:39:08 +0100

    New way

--- Zawadi/hosts.py
+++ Zawadi/hosts.py
@@ -3,6 +3,6 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('', 
     host(r'vendeur', 'Zawadi.urls', name="seller"),
-    host(r'zawadi', 'Zawadi.urls', name="seller"),
+    host(r'zawadi', 'Zawadi.urls', name="test_seller"),
     host(r'', 'app.urls_client', name="achat")
 )
