930d15f9be8a9693c2775735d6bac544f8df4ae7
georis <georiseno@gmail.com>
2023-03-21 07:35:46 +0100

    new

--- Zawadi/hosts.py
+++ Zawadi/hosts.py
@@ -6,5 +6,6 @@ host_patterns = patterns('',
     host(r'zawadi', 'Zawadi.urls', name="test_seller"),
     host(r'api', 'app.urls_client', name="api"),
     host(r'v1', 'app.urls_client', name="achat"),
+    host(r'', 'app.urls_client', name="backup"),
     host(r'blog', 'blog.urls', name="blog")
 )
